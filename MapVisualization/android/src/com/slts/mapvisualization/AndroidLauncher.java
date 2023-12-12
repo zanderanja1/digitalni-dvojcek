4e0c6b3d9a67dd2df09fab62519875b45c06cbec
zan <deranja.zan@gmail.com>
2023-12-12 12:54:32 +0100

    SCRUM-5

---
+++ MapVisualization/android/src/com/slts/mapvisualization/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.slts.mapvisualization;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.slts.mapvisualization.MapVisualization;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MapVisualization(), config);
+	}
+}
