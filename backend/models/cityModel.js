d5e1fc1e74c2674bd2a49095bd372c72624fcdda
JakobKrajnc <jakob.krajnc@student.um.si>
2024-01-08 14:38:27 +0100

    Fetch city coordinates with scraper

--- backend/models/cityModel.js
+++ backend/models/cityModel.js
@@ -1,35 +1,39 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var citySchema = new Schema({
+  name: String,
+  temperature: String,
+  humidity: String,
+  wind: String,
+  weatherStatus: String,
+  weather: {
+    byHour: [
+      {
+        hourTime: Array,
+        hourTemperature: Array,
+        hourHumidity: Array,
+        hourWind: Array,
+        hourWeatherStatus: Array
+      }
+    ],
+    byDay: [
+      {
+        dayTime: Array,
+        dayTemperature: Array,
+        dayHumidity: Array,
+        dayWind: Array,
+        dayWeatherStatus: Array
+      }
+    ]
+  },
 
-	'name' : String,
-	'temperature' : String,
-	'humidity' : String,
-	'wind' : String,
-	'weatherStatus': String,
-	'weather': {
-		'byHour': [{
-			'hourTime':Array,
-			'hourTemperature':Array,
-			'hourHumidity':Array,
-			'hourWind':Array,
-			'hourWeatherStatus':Array
-		}],
-		'byDay': [{
-			'dayTime':Array,
-			'dayTemperature':Array,
-			'dayHumidity':Array,
-			'dayWind':Array,
-			'dayWeatherStatus':Array
-		}],
-	},
-		
-	'favouritesBy': {
-		type: [Schema.Types.ObjectId],
-		ref: 'user',
-	}
-
+  favouritesBy: {
+    type: [Schema.Types.ObjectId],
+    ref: "user"
+  },
+  latitude: String,
+  longitude: String
 });
 
-module.exports = mongoose.model('city', citySchema);
+module.exports = mongoose.model("city", citySchema);
